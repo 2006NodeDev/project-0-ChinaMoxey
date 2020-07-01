cb432b14317ded4374d34759eb2df51585c7464f
ChinaMoxey <moxeychina@gmail.com>
2020-07-01 00:59:59 -0400

    updated response

--- src/middleware/authentication-middleware.ts
+++ src/middleware/authentication-middleware.ts
@@ -2,7 +2,7 @@ import{Response, NextFunction} from 'express'
 
 export function authenticationMiddleware(req:any, res:Response, next:NextFunction){
     if(!req.session.user){
-        res.status(401).send('User needs to login it')
+        res.status(401).send('User needs to login in')
     }else{
         console.log(`user ${req.session.user.username} has a role of ${req.session.user.role}`);
         next()
